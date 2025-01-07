493fbd01fcaa8b9caf07eb89b46ab755f47b1325
Newton <dev.newton.negri@gmail.com>
2025-01-07 17:16:51 -0300

    Atualiza configuração do Angular para novo caminho de saída e ajusta título da aplicação

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/portal/p/1/teste'
+    }
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'fluig-tb-alteracao-registro-app';
 }
