c869a1875f346881f225e98949bbedfa4e16d6d6
Newton <dev.newton.negri@gmail.com>
2025-01-10 09:36:47 -0300

    Revert "Atualiza configuração do Angular para novo caminho de saída e ajusta título da aplicação"
    This reverts commit 493fbd01fcaa8b9caf07eb89b46ab755f47b1325.

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  providers: [
-    {
-      provide: APP_BASE_HREF,
-      useValue: '/portal/p/1/teste'
-    }
-  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fluig-tb-alteracao-registro-app';
+  title = 'angular-app';
 }
