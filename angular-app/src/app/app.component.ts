470fafd2a17efa1ba7da2f570709b14e435dea55
Newton <dev.newton.negri@gmail.com>
2025-01-10 14:55:11 -0300

    Adiciona Angular Material e CDK ao projeto, implementa formulário com validação e estilização usando mat-card

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
   providers: [
     {
       provide: APP_BASE_HREF,
@@ -17,4 +26,7 @@ import { APP_BASE_HREF } from '@angular/common';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  // Cria um FormControl para o campo de token
+  tokenFormControl = new FormControl('', [Validators.required]);
 }
