545fa85ed58032783b0d0eefdf9786b1030c8e95
Newton Negri <dev.newton.negri.com>
2025-03-10 18:04:15 -0300

    Atualiza lógica de controle de processos para incluir novos métodos de geração de token e limpeza de campos de aprovação, além de restaurar arquivos de processamento.

--- forms/HMO_Alteração de Registro/afterStateLeave.js
+++ forms/HMO_Alteração de Registro/afterStateLeave.js
@@ -8,17 +8,10 @@ function afterStateLeave(sequenceId) {
    // Verifica se o sequenceId é igual à variável AJUSTAR
    if (sequenceId == AJUSTAR) {
       // Verifica o valor do campo "ativAprovStatusJuridico_014"
-      var statusJuridico = hAPI.getCardValue("ativAprovStatusJuridico_014");
-      if (statusJuridico == "AJUSTAR") {
+      var statusVerificaRh = hAPI.getCardValue("ativAprovStatusVerificaRh");
+      if (statusVerificaRh == "AJUSTAR") {
          // Se for "AJUSTAR", limpa o campo
-         hAPI.setCardValue("ativAprovStatusJuridico_014", "");
-      }
-
-      // Verifica o valor do campo "ativAprovStatusRh_014"
-      var statusRh = hAPI.getCardValue("ativAprovStatusRh_014");
-      if (statusRh == "AJUSTAR") {
-         // Se for "AJUSTAR", limpa o campo
-         hAPI.setCardValue("ativAprovStatusRh_014", "");
+         hAPI.setCardValue("ativAprovStatusVerificaRh", "");
       }
    }
 }
